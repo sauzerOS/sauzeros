02ae79c902ce3cad9ad5480a936edc52d8de17cb
fanboimsft <fanboimsft@icloud.com>
2021-07-16 08:49:56 +0000

    clean repo

---
+++ core/kiss/files/kiss_path.sh
@@ -0,0 +1,1 @@
+export KISS_PATH=/repo/no_updates:/repo/sauzeros/core:/repo/sauzeros/extra:/repo/sauzeros/xorg:/repo/kiss/core:/repo/kiss/extra:/repo/kiss/wayland:/repo/community/community:/repo/sauzeros/bloat
