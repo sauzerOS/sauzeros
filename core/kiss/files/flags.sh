30a8b9cd7d865fe131f749be3ab3d42d7c6de5a4
fanboimsft <fanboimsft@icloud.com>
2023-02-17 10:09:56 +0000

    init

---
+++ core/kiss/files/flags.sh
@@ -0,0 +1,13 @@
+export CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe"
+export CXXFLAGS=$CFLAGS
+export MAKEFLAGS="-j$(($(nproc)+2))"
+export KISS_HOOK=/etc/kiss-hook
+export KISS_SU=su
+export KISS_TMPDIR=/tmp
+export KISS_COMPRESS=zst
+export XZ_OPT="-T0"
+export $(cat /etc/locale.conf)
+export EDITOR=nano
+
+alias ls='ls --color'
+alias grep='grep --color=auto'
