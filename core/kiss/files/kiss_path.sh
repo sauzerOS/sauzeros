127421f520484cd8cf896eb1c77eef1d5b64e9aa
fanboimsft <fanboimsft@icloud.com>
2023-02-18 15:14:13 +0700

    base files

--- core/kiss/files/kiss_path.sh
+++ core/kiss/files/kiss_path.sh
@@ -1,1 +1,1 @@
-export KISS_PATH=/repo/no_updates:/repo/sauzeros/core:/repo/sauzeros/extra:/repo/sauzeros/xorg:/repo/sauzeros/xfce4:/repo/kiss/core:/repo/kiss/extra:/repo/kiss/wayland:/repo/community/community
+export KISS_PATH=/repo/no_updates:/repo/sauzeros/core:/repo/sauzeros/extra:/repo/sauzeros/xorg:/repo/sauzeros/xfce4
