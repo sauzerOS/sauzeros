4d019d3751c8c906c1be799595ce9fefbdeeba14
fanboimsft <fanboimsft@icloud.com>
2024-02-20 22:29:38 +0700

    updates

--- core/kiss/files/flags.sh
+++ core/kiss/files/flags.sh
@@ -1,7 +1,7 @@
-export CFLAGS="-march=native -mtune=native -O2 -pipe"
+export CFLAGS="-march=native -mtune=native -O2 -pipe -fomit-frame-pointer -flto -ftree-vectorize"
 export CXXFLAGS=$CFLAGS
 export MAKEFLAGS="-j$(($(nproc)+2))"
-export KISS_SU=su
+export KISS_SU=sudo
 export KISS_TMPDIR=/tmp
 export KISS_COMPRESS=zst
 export XZ_OPT="-T0 -M 90%"
