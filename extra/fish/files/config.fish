a9d996739de34289565e7829b74d7a0a7e05edbf
fanboimsft <fanboimsft@icloud.com>
2025-09-12 01:23:57 +0700

    bash: 5.3 → 5.3.3 bc: 7.0.3 → 7.1.0 curl: 8.15.0 → 8.16.0 glib: 2.84.4 → 2.86.0 kiss: 0.3 → 0.5 libxml2: 2.14.5 → 2.14.6 linux-headers: 6.16.5 → 6.16.6 p11-kit: 0.25.5 → 0.25.7 readline: 8.3 → 8.3p001 zlib: 1.3.1 → 2.2.5 freetype: 2.13.3 → 2.14.0 go: 1.24.6 → 1.25.1 google-chrome: 140.0.7339.80 → 140.0.7339.127 libXres: 1.2.2 → 1.2.3 libaom: 3.12.1 → 3.13.1 libproxy: 0.5.10 → 0.5.11 librsvg: 2.61.0 → 2.61.1 libxpresent: 1.0.1 → 1.0.2 lvm2: 2.03.34 → 2.03.35 nghttp2: 1.66.0 → 1.67.0 nodejs: 22.19.0 → 24.7.0 poppler: 25.08.0 → 25.09.1 python-feedparser: 6.0.11 → 6.0.12 python-setuptools-scm: 8.3.1 → 9.2.0 python-trove-classifiers: 2025.8.26.11 → 2025.9.9.12 python-werkzeug: 2.2.3 → 3.1.3 rofi: 2.0.0b1 → 2.0.0 syncthing: 2.0.7 → 2.0.8 tcl: 8.6.16 → 9.0.2 tk: 8.6.16 → 9.0.2
    updates

---
+++ extra/fish/files/config.fish
@@ -0,0 +1,6 @@
+set -gx CFLAGS "-march=native -mtune=native -O2 -pipe -fomit-frame-pointer"
+set -gx CXXFLAGS $CFLAGS
+set -gx MAKEFLAGS "-j$(nproc)"
+set -gx XZ_OPT "-T0 -M 90%"
+set -gx EDITOR nano
+set -gx fish_user_paths /usr/local/bin /opt/cuda/bin $fish_user_paths
