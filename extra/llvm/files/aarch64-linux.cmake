d40d42766ee0edfe2a56dc117a549610a7719982
fanboimsft <fanboimsft@icloud.com>
2025-12-19 14:41:44 +0700

    fix bump

--- extra/llvm/files/aarch64-linux.cmake
+++ extra/llvm/files/aarch64-linux.cmake
@@ -6,8 +6,8 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 
-set(CMAKE_SYSROOT /var/tmp/sauzeros-rpi4)
-set(CMAKE_FIND_ROOT_PATH /var/tmp/sauzeros-rpi4)
+set(CMAKE_SYSROOT /mnt/vdrlakepi)
+set(CMAKE_FIND_ROOT_PATH /mnt/vdrlakepi)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
